d72a6d52a4cf1a97662997070aca5c2cb28caafa
Robert Marchetti <automationsmith@gmail.com>
2022-02-24 07:31:25 -0500

    added to demo test which aloow you to copy from the clipboad to notepad, this is good for triggering clipboard copy events

---
+++ psutil_test.py
@@ -0,0 +1,12 @@
+"""Quick Example of getting a list of PID and name works on mac and Windows"""
+
+import psutil
+
+
+# Print a list of pid's and names
+process = psutil.process_iter()
+for i in process:
+    print(i.name, i.pid)
+
+
+
