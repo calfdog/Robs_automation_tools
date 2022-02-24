4904f48e4d7c7f6c36e15d2cda5b8ae11cfc0034
Robert Marchetti <automationsmith@gmail.com>
2022-02-24 07:31:25 -0500

    added to quick example

---
+++ name_data.py
@@ -0,0 +1,24 @@
+
+text = ["Name: Joe Douglas\n",
+        "Age: 60\n",
+        "ID: a5057c34dc\n",
+        "State: MA\n",
+        "Date: 07/02/1978\n\n",
+        "Name: Bill Smith\n",
+        "Age: 25\n",
+        "ID: b5057c34dc\n",
+        "State: RI\n",
+        "Date: 01/02/1968\n\n",
+        "Name:Lionel Jax\n",
+        "Age: 45\n",
+        "ID: c5057c34dc\n",
+        "State: NH\n",
+        "Date: 01/02/1989\n\n"
+        "Name: Mary Jax\n",
+        "Age: 45\n",
+        "ID: c5057c34dc\n",
+        "State: NH\n",
+        "Date: 07/12/1989\n"]
+
+
+
---
+++ psutil_test.py
@@ -0,0 +1,13 @@
+"""Quick Example of getting a list of PID and name works on mac and Windows"""
+
+import psutil
+
+
+
+# Print a list of pid's and names
+process = psutil.process_iter()
+for i in process:
+    print(i.name, i.pid)
+
+
+
