4c19ccef3e7b187301fe447e80d4f78e708e1225
Rob Marchetti <automationsmith@gmail.com>
2022-03-18 02:43:30 -0400

    used for generating names or for emails

--- name_gen.py
+++ name_gen.py
@@ -7,12 +7,12 @@ import random
 
 
 def get_name():
-    file = 'last_names.txt'
+    file = 'data/last_names.txt'
     names_file = open(file, "r")
     lnames = names_file.read()
     last_names = [lname for lname in lnames.split('\n')]
 
-    file = 'first_names.txt'
+    file = 'data/first_names.txt'
     names_file = open(file, "r")
     fnames = names_file.read()
     first_names = [fname for fname in fnames.split('\n')]
