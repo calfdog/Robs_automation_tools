87e0dc86f0b54018a02f81a5e854f12a3a32067c
Robert Marchetti <automationsmith@gmail.com>
2022-03-30 06:40:25 -0400

    refactor moving for better organizing to appropriate directories

--- misc_utils/person.py
+++ misc_utils/person.py
@@ -11,7 +11,7 @@ import string
 __author__ = 'Rmarchetti'
 
 
-class utilities():
+class Person():
     first_names = ["James", "Christopher", "Ronald", "Mary", "Lisa", "Michelle", "John", "Daniel", "Anthony", "Patricia", "Nancy", "Laura", "Robert", "Paul", "Kevin", "Linda", "Karen", "Sarah", "Michael", "Mark", "Jason", "Barbara", "Betty", "Kimberly", "William", "Donald", "Jeff", "Elizabeth", "Helen", "Deborah", "David", "George", "Jennifer", "Sandra", "Richard", "Kenneth", "Maria", "Donna", "Charles", "Steven", "Susan", "Carol", "Joseph", "Edward", "Margaret", "Ruth", "Thomas", "Brian", "Dorothy", "Sharon"]
     last_names = ["Smith", "Anderson", "Clark", "Wright", "Mitchell", "Johnson", "Thomas", "Rodriguez", "Lopez", "Perez", "Williams", "Jackson", "Lewis", "Hill", "Roberts", "Jones", "White", "Lee", "Scott", "Turner", "Brown", "Harris", "Walker", "Green", "Phillips", "Davis", "Martin", "Hall", "Adams", "Campbell", "Miller", "Thompson", "Allen", "Baker", "Parker", "Wilson", "Garcia", "Young", "Gonzalez", "Evans", "Moore", "Martinez", "Hernandez", "Nelson", "Edwards", "Taylor", "Robinson", "King", "Carter", "Collins"]
     sex = {"Female": "F", "Male Impersonator": "G", "Male": "M", "Female Impersonator": "N", "Male Name, No Gender Given": "Y", "Female Name, No Gender Given": "Z", "Unknown": "X"}
